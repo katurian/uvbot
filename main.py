903143021fdfd5a822cc481af80de0fe95ed33a5
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2023-07-27 23:06:10 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+import re
+import discord
+import requests
+from datetime import datetime
+import json
+from urllib.parse import quote
+
+def get_uv_color(uv_value):
+    if uv_value <= 2:
+        return "rgba(0, 255, 0, 0.6)" # green
+    elif uv_value <= 5:
+        return "rgba(255, 255, 0, 0.6)" # yellow
+    elif uv_value <= 7:
+        return "rgba(255, 165, 0, 0.6)" # orange
+    elif uv_value <= 10:
+        return "rgba(255, 0, 0, 0.6)" # red
+    else:
+        return "rgba(238, 130, 238, 0.6)" # violet
+
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Beep Boop, I am uvbot!')
+    print("------")
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if '?uv' in message.content:
+        labels = []
+        uv_values = []
+        zipcode = re.search(r'\d{5}', message.content).group(0)
+        response = requests.get(f'https://data.epa.gov/efservice/getEnvirofactsUVHourly/ZIP/{zipcode}/JSON')
+        uv_json = response.json()
+        for item in uv_json:  
+            dt_obj = datetime.strptime(item['DATE_TIME'], '%b/%d/%Y %I %p')
+            labels.append(dt_obj.strftime('%I %p'))
+            uv_values.append(int(item['UV_VALUE']))
+        uv_colors = [get_uv_color(value) for value in uv_values]
+        labels = labels[2:-7]
+        uv_values = uv_values[2:-7]
+        uv_colors = uv_colors[2:-7]
+        chart_config = {
+            "type": "bar",
+            "data": {
+                "labels": labels,
+                "datasets": [ 
+                    {
+                        "label": "UV Index",
+                        "data": uv_values,
+                        "backgroundColor": uv_colors
+                    }
+                ],
+            },
+            "options": {"legend": {"display": False}, "title": {"display": True, "text": zipcode}}
+        }
+        chart_config = json.dumps(chart_config, indent=2)
+        postdata = {
+          'chart': chart_config,
+          'width': 500,
+          'height': 300,
+          'backgroundColor': 'white',
+        }
+        response = requests.post('https://quickchart.io/chart/create', json=postdata)
+        parsed = json.loads(response.text)
+        await message.reply(parsed["url"])
+    if message.content == '<:ching_cat:1134237022228324352>': 
+        await message.reply('<:ching_cat:1134237022228324352>')
+
+client.run('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
